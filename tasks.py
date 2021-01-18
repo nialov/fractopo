f553a186bb9543c3574d6442b8cfefae966565b4
nialov <nikolasovaskainen@hotmail.com>
2021-01-18 21:09:31 +0200

    add invoke tasks

---
+++ tasks.py
@@ -0,0 +1,54 @@
+"""
+Invoke tasks.
+"""
+from invoke import task
+
+
+nox_tests = (
+    "tests_strict",
+    "tests_lazy",
+    "test_tracevalidate",
+    "rstcheck_docs",
+    "docs",
+)
+
+
+@task
+def requirements(c):
+    """
+    Sync requirements from Pipfile to requirements*.txt and setup.py.
+    """
+    c.run(
+        "pipenv run pipenv_to_requirements "
+        "-o docs_src/requirements.txt -d docs_src/requirements-dev.txt"
+    )
+    c.run("pipenv run pipenv-setup sync --pipfile --dev")
+
+
+@task
+def nox(c):
+    """
+    Run whole nox suite synchronously.
+    """
+    c.run("nox --no-color")
+
+
+@task
+def nox_parallel(c):
+    """
+    Run nox suite in parallel.
+    """
+    promises = [
+        c.run(f"nox --session {nox_test} --no-color", asynchronous=True, timeout=240,)
+        for nox_test in nox_tests
+    ]
+    for promise in promises:
+        promise.join()
+
+
+@task(pre=[requirements, nox])
+def make(_):
+    """
+    Test and make everything.
+    """
+
