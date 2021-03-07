0a13263917ca5ebcc36615a51e7080495340bd29
nialov <nikolasovaskainen@hotmail.com>
2021-03-07 19:52:31 +0200

    rm test_getaberget_fault_network

--- tests/analysis/test_network.py
+++ tests/analysis/test_network.py
@@ -3,11 +3,11 @@ Tests for Network.
 """
 from typing import Tuple
 
-import geopandas as gpd
 import numpy as np
 import pandas as pd
 import pytest
 from shapely.geometry import MultiPolygon, Polygon
+import geopandas as gpd
 
 from fractopo import SetRangeTuple
 from fractopo.analysis.network import Network
@@ -99,7 +99,7 @@ def test_network(
         [isinstance(val, (Polygon, MultiPolygon)) for val in network.target_areas]
     )
 
-    if network.branch_gdf.shape[0] < 2500:
+    if network.branch_gdf.shape[0] < 500:
         # Do not check massive branch counts
         file_regression.check(network.branch_gdf.sort_index().to_json())
 
@@ -193,36 +193,22 @@ def test_network_circular_target_area(trace_gdf, area_gdf, name):
     )
 
 
-def test_getaberget_fault_network(data_regression):
-    """
-    Debug test with material causing unary_union fault.
-    """
-    traces = Helpers.unary_err_traces
-    areas = Helpers.unary_err_areas
-    assert isinstance(traces, gpd.GeoDataFrame)
-    assert isinstance(areas, gpd.GeoDataFrame)
+# def test_getaberget_fault_network():
+#     """
+#     Debug test with material causing unary_union fault.
+#     """
+#     traces = Helpers.unary_err_traces.sample(frac=0.1)
+#     areas = Helpers.unary_err_areas
+#     assert isinstance(traces, gpd.GeoDataFrame)
+#     assert isinstance(areas, gpd.GeoDataFrame)
 
-    network = Network(
-        trace_gdf=traces,
-        area_gdf=areas,
-        determine_branches_nodes=True,
-        truncate_traces=True,
-        snap_threshold=0.001,
-        unary_size_threshold=13000,
-    )
+#     network = Network(
+#         trace_gdf=traces,
+#         area_gdf=areas,
+#         determine_branches_nodes=True,
+#         truncate_traces=True,
+#         snap_threshold=0.001,
+#         unary_size_threshold=13000,
+#     )
 
-    network_attributes = dict()
-    for attribute in ("node_counts", "branch_counts"):
-        # network_attributes[attribute] = getattr(network, attribute)
-        for key, value in getattr(network, attribute).items():
-            network_attributes[key] = int(value)
-
-        for key, value in network.numerical_network_description().items():
-            try:
-                network_attributes[key] = value.item()
-            except AttributeError:
-                network_attributes[key] = value
-
-    data_regression.check(network_attributes)
-
-    assert network.branch_gdf.shape[0] > network.trace_gdf.shape[0]
+#     assert network.branch_gdf.shape[0] >= network.trace_gdf.shape[0]
