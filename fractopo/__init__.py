bd21265895e1ee3070b332d39d1f837cf2de59e0
nialov <nikolasovaskainen@hotmail.com>
2021-07-14 16:44:40 +0300

    build: ignore

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -8,7 +8,7 @@ import logging
 from fractopo.analysis.network import Network
 from fractopo.tval.trace_validation import Validation
 
-__version__ = "0.0.1.post259.dev0+d231b7a"
+__version__ = "0.0.1.post265.dev0+5cb65f2"
 
 
 logging.info(f"Main imports available from fractopo/__init__.py: {Network, Validation}")
