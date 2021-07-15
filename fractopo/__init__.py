6880d5417cf7be027d3f347b000f82b8b30d5180
nialov <nikolasovaskainen@hotmail.com>
2021-07-15 15:47:36 +0300

    docs: test and execute notebooks

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -8,7 +8,7 @@ import logging
 from fractopo.analysis.network import Network
 from fractopo.tval.trace_validation import Validation
 
-__version__ = "0.0.1.post273.dev0+06a3892"
+__version__ = "0.0.1.post277.dev0+27d30df"
 
 
 logging.info(f"Main imports available from fractopo/__init__.py: {Network, Validation}")
