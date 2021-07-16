efc73f5519240b773a6c9fa5b99cc57249be8fd3
nialov <nikolasovaskainen@hotmail.com>
2021-07-16 15:41:23 +0300

    docs: update versio

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -8,7 +8,7 @@ import logging
 from fractopo.analysis.network import Network
 from fractopo.tval.trace_validation import Validation
 
-__version__ = "0.0.1.post277.dev0+27d30df"
+__version__ = "0.0.1.post281.dev0+15b8e8d"
 
 
 logging.info(f"Main imports available from fractopo/__init__.py: {Network, Validation}")
