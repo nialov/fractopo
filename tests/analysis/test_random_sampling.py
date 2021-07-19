8e990fb87ca2e596834d05c4b0afb12192b5bc67
nialov <nikolasovaskainen@hotmail.com>
2021-07-19 14:15:02 +0300

    test: fix random sampling tests to match new functionality

--- tests/analysis/test_random_sampling.py
+++ tests/analysis/test_random_sampling.py
@@ -25,6 +25,7 @@ def test_network_random_sampler_manual():
         min_radius=min_radius,
         snap_threshold=snap_threshold,
         random_choice=RandomChoice.radius,
+        name="geta1",
     )
     circle_samples = []
     assert sampler.max_radius > min_radius
@@ -40,7 +41,12 @@ def test_network_random_sampler_manual():
         circle_samples.append(random_target_circle)
 
     for _ in range(samples):
-        network, target_centroid, radius = sampler.random_network_sample()
+        random_sample = sampler.random_network_sample()
+        network, target_centroid, radius = (
+            random_sample.network_maybe,
+            random_sample.target_centroid,
+            random_sample.radius,
+        )
         assert isinstance(network, Network)
         if not network.trace_gdf.shape[0] <= trace_gdf.shape[0]:
             assert np.isclose(radius, sampler.max_radius)
@@ -48,8 +54,7 @@ def test_network_random_sampler_manual():
 
         # TODO: Errors are possible and will randomly occur.
         assert (
-            sum(network.branch_types == Error_branch) / len(network.branch_types)
-            < 0.0001
+            sum(network.branch_types == Error_branch) / len(network.branch_types) < 0.01
         )
     return circle_samples
 
@@ -70,6 +75,7 @@ def test_network_random_sampler(
         min_radius=min_radius,
         snap_threshold=snap_threshold,
         random_choice=random_choice,
+        name="sampler",
     )
     assert sampler.max_radius > min_radius
     for _ in range(100):
@@ -83,7 +89,12 @@ def test_network_random_sampler(
         assert random_target_circle.area < sampler.target_circle.area
 
     for _ in range(samples):
-        network, target_centroid, radius = sampler.random_network_sample()
+        random_sample = sampler.random_network_sample()
+        network, target_centroid, radius = (
+            random_sample.network_maybe,
+            random_sample.target_centroid,
+            random_sample.radius,
+        )
         assert isinstance(network, Network)
         if not network.trace_gdf.shape[0] <= trace_gdf.shape[0]:
             assert np.isclose(radius, sampler.max_radius)
