9ec75ef5d394dab4624329ae4917b2f6cc90e55f
nialov <nikolasovaskainen@hotmail.com>
2021-07-19 15:13:27 +0300

    refactor: move subsampling mostly to subsampling.py

--- fractopo/analysis/multi_network.py
+++ fractopo/analysis/multi_network.py
@@ -5,9 +5,9 @@ MultiNetwork implementation for handling multiple network analysis.
 from dataclasses import dataclass
 from typing import Sequence
 
-from fractopo import general
+from fractopo.analysis import subsampling
 from fractopo.analysis.network import Network
-from fractopo.analysis.random_sampling import NetworkRandomSampler, RandomChoice
+from fractopo.analysis.random_sampling import RandomChoice
 
 
 @dataclass
@@ -19,13 +19,6 @@ class MultiNetwork:
 
     networks: Sequence[Network]
 
-    @staticmethod
-    def _create_sample(sampler: NetworkRandomSampler):
-        """
-        Sample with ``NetworkRandomSampler``.
-        """
-        return sampler.random_network_sample()
-
     def subsample(
         self,
         min_radius: float,
@@ -33,23 +26,11 @@ class MultiNetwork:
         samples: int = 1,
     ):
         """
-        Subsample all Networks.
+        Subsample all ``Networks`` in ``MultiNetwork``.
         """
-        assert isinstance(samples, int)
-        assert samples > 0
-
-        subsamplers = [
-            NetworkRandomSampler.random_network_sampler(
-                network=network, min_radius=min_radius, random_choice=random_choice
-            )
-            for network in self.networks
-        ]
-
-        subsamples = general.multiprocess(
-            function_to_call=self._create_sample,
-            keyword_arguments=subsamplers,
-            arguments_identifier=lambda sampler: sampler.name,
-            repeats=samples - 1,
+        return subsampling.subsample_networks(
+            networks=self.networks,
+            min_radius=min_radius,
+            random_choice=random_choice,
+            samples=samples,
         )
-
-        return subsamples
---
+++ fractopo/analysis/subsampling.py
@@ -0,0 +1,71 @@
+"""
+Utilities for Network subsampling.
+"""
+from typing import Dict, List, Sequence, Union
+
+import pandas as pd
+
+from fractopo import general
+from fractopo.analysis.network import Network
+from fractopo.analysis.random_sampling import (
+    NetworkRandomSampler,
+    RandomChoice,
+    RandomSample,
+)
+from fractopo.general import Number
+
+
+def create_sample(sampler: NetworkRandomSampler):
+    """
+    Sample with ``NetworkRandomSampler``.
+    """
+    return sampler.random_network_sample()
+
+
+def subsample_networks(
+    networks: Sequence[Network],
+    min_radius: float,
+    random_choice: RandomChoice = RandomChoice.radius,
+    samples: int = 1,
+) -> List[general.ProcessResult]:
+    """
+    Subsample given Sequence of Networks.
+    """
+    assert isinstance(samples, int)
+    assert samples > 0
+
+    subsamplers = [
+        NetworkRandomSampler.random_network_sampler(
+            network=network, min_radius=min_radius, random_choice=random_choice
+        )
+        for network in networks
+    ]
+
+    # Gather subsamples with multiprocessing
+    subsamples = general.multiprocess(
+        function_to_call=create_sample,
+        keyword_arguments=subsamplers,
+        arguments_identifier=lambda sampler: sampler.name,
+        repeats=samples - 1,
+    )
+
+    return subsamples
+
+
+def gather_subsample_descriptions(
+    subsample_results: List[general.ProcessResult],
+) -> pd.DataFrame:
+    """
+    Gather results for a list of subsampling ProcessResults.
+    """
+    descriptions = []
+    for subsample in subsample_results:
+        random_sample = subsample.result
+        assert isinstance(random_sample, RandomSample)
+        if random_sample.network_maybe is None:
+            continue
+        else:
+            description = random_sample.network_maybe.numerical_network_description()
+
+        descriptions.append(description)
+    return pd.DataFrame(descriptions)
