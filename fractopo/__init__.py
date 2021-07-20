948f6636621cbc519e1f9ddc748a8bded7d437aa
nialov <nikolasovaskainen@hotmail.com>
2021-07-20 15:56:53 +0300

    build: update dependencies by releasing pygeos lock

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -9,7 +9,7 @@ from fractopo.analysis.multi_network import MultiNetwork
 from fractopo.analysis.network import Network
 from fractopo.tval.trace_validation import Validation
 
-__version__ = "0.0.1.post312.dev0+fc92755"
+__version__ = "0.0.1.post313.dev0+6e57a2f"
 
 
 logging.info(
