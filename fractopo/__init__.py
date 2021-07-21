c9630e9c1c8c27b479dd397c56b23a8c61358134
nialov <nikolasovaskainen@hotmail.com>
2021-07-21 12:11:37 +0300

    docs: execute notebooks

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -9,7 +9,7 @@ from fractopo.analysis.multi_network import MultiNetwork
 from fractopo.analysis.network import Network
 from fractopo.tval.trace_validation import Validation
 
-__version__ = "0.0.1.post328.dev0+bd02bbb"
+__version__ = "0.0.1.post329.dev0+7851967"
 
 
 logging.info(
