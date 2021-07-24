cdc2737efeedeafca1351448bade6f068f24784f
nialov <nikolasovaskainen@hotmail.com>
2021-07-24 15:55:52 +0300

    docs: update version

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -9,7 +9,7 @@ from fractopo.analysis.multi_network import MultiNetwork
 from fractopo.analysis.network import Network
 from fractopo.tval.trace_validation import Validation
 
-__version__ = "0.0.1.post351.dev0+015284e"
+__version__ = "0.0.1.post355.dev0+a21946a"
 
 
 logging.info(
