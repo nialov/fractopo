437ed1dd5297403db61079347f16d4caf400ccbe
nialov <nikolasovaskainen@hotmail.com>
2021-07-26 19:06:24 +0300

    docs: update version and badge

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -9,7 +9,7 @@ from fractopo.analysis.multi_network import MultiNetwork
 from fractopo.analysis.network import Network
 from fractopo.tval.trace_validation import Validation
 
-__version__ = "0.0.1.post361.dev0+915fd1e"
+__version__ = "0.0.1.post365.dev0+287a5c3"
 
 
 logging.info(
