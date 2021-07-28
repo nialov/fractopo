d65be7b3f5e65228018df7998e2426ae56619203
nialov <nikolasovaskainen@hotmail.com>
2021-07-28 09:27:29 +0300

    docs: make short network example

--- examples/plot_data.py
+++ examples/plot_data.py
@@ -1,24 +1,78 @@
 """
-Plotting the base data that fractopo uses
-=========================================
+Creating a fractopo network from trace data
+===========================================
 
-Rather than showing ``fractopo`` functionality the functions of the underlying
-libraries powering ``fractopo`` are introduced here.
+Data is loaded into ``fractopo`` using ``geopandas`` which can load from
+a wide variety of sources.
 """
 
 # %%
+# Here we load data from the internet from the ``fractopo`` GitHub repository.
 
-# L
-myvariable = 2
-print("my variable is {}".format(myvariable))
-# This is the end of the 'code block' (if using an above IDE). All code within
-# this block can be easily executed all at once.
+import geopandas as gpd
+
+trace_data_url = (
+    "https://raw.githubusercontent.com/nialov/"
+    "fractopo/master/tests/sample_data/KB11/KB11_traces.geojson"
+)
+area_data_url = (
+    "https://raw.githubusercontent.com/nialov/"
+    "fractopo/master/tests/sample_data/KB11/KB11_area.geojson"
+)
+
+# Use geopandas to load data from urls
+traces = gpd.read_file(trace_data_url)
+area = gpd.read_file(area_data_url)
+
+# Check that the type is GeoDataFrame
+assert isinstance(traces, gpd.GeoDataFrame)
+assert isinstance(area, gpd.GeoDataFrame)
+
+# Name the dataset
+name = "KB11"
 
 # %%
-# This is another section header
-# ------------------------------
-#
-# In the built documentation, it will be rendered as rST after the code above!
-# This is also another code block.
+# Plotting the loaded data
+# ------------------------
 
-print("my variable plus 2 is {}".format(myvariable + 2))
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Initialize the figure and ax in which data is plotted
+fig, ax = plt.subplots(figsize=(9, 9))
+
+# Plot the loaded trace dataset consisting of fracture traces.
+traces.plot(ax=ax, color="blue")
+
+# Plot the loaded area dataset that consists of a single polygon
+# that delineates the traces.
+area.boundary.plot(ax=ax, color="red")
+
+# Give the figure a title
+ax.set_title(f"{name}, Coordinate Reference System = {traces.crs}")
+
+# %%
+# Creating a fractopo Network
+# ---------------------------
+
+# Import Network class from fractopo
+from fractopo import Network
+
+kb11_network = Network(
+    trace_gdf=traces,
+    area_gdf=area,
+    truncate_traces=True,
+    circular_target_area=False,
+    determine_branches_nodes=True,
+    snap_threshold=0.001,
+)
+
+# %%
+# Plotting using the Network
+# --------------------------
+
+# Rose plot of network branch orientations
+kb11_network.plot_branch_azimuth()
+
+# XYI-plot of topological node counts
+kb11_network.plot_xyi()
