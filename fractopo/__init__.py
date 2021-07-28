97a77c9a32325a0e5da2af0df0a13b95b4855c32
nialov <nikolasovaskainen@hotmail.com>
2021-07-28 15:42:11 +0300

    docs: update version

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -9,7 +9,7 @@ from fractopo.analysis.multi_network import MultiNetwork
 from fractopo.analysis.network import Network
 from fractopo.tval.trace_validation import Validation
 
-__version__ = "0.0.1.post372.dev0+d65be7b"
+__version__ = "0.0.1.post378.dev0+efe5bee"
 
 
 logging.info(
