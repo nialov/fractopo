7afd6fc3d44adbfea3aaafd9e0317458732886e6
nialov <nikolasovaskainen@gmail.com>
2022-02-23 17:25:57 +0200

    docs: update deps, version and changelog

--- fractopo/__init__.py
+++ fractopo/__init__.py
@@ -16,7 +16,7 @@ from fractopo.analysis.multi_network import MultiNetwork  # noqa: E402
 from fractopo.analysis.network import Network  # noqa: E402
 from fractopo.tval.trace_validation import Validation  # noqa: E402
 
-__version__ = "0.3.0"
+__version__ = "0.3.0.post12.dev0+8556f12"
 
 
 logging.info(
