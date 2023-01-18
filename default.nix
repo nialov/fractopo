710eeb978501791feb853ee04986ac9ab6286f48
nialov <nikolasovaskainen@gmail.com>
2023-01-18 12:34:56 +0200

    build(default): clean src

--- default.nix
+++ default.nix
@@ -83,7 +83,29 @@ let
 in buildPythonPackage {
   pname = "fractopo";
   version = "0.5.2";
-  src = poetry2nix.cleanPythonSources { src = ./.; };
+  src = let
+    # Filter from src paths with given suffixes (full name can be given as suffix)
+    excludeSuffixes = [
+      ".flake8"
+      ".nix"
+      "docs_src"
+      "flake.lock"
+      "noxfile.py"
+      "dodo.py"
+      "examples"
+      "environment.yml"
+      "paper"
+    ];
+    # anyMatches returns true if any suffix matches
+    anyMatches = path:
+      (builtins.any (value: lib.hasSuffix value (baseNameOf path))
+        excludeSuffixes);
+
+  in builtins.filterSource (path: type:
+    # Apply the anyMatches filter and reverse the result with !
+    # as we want to EXCLUDE rather than INCLUDE
+    !(anyMatches path)) (poetry2nix.cleanPythonSources { src = ./.; });
+  # src = poetry2nix.cleanPythonSources { src = ./.; };
   format = "pyproject";
 
   # Uses poetry for install
