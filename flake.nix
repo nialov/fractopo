0764edccfeaf50927f35ff85565bb4b4b813fd79
nialov <nikolasovaskainen@gmail.com>
2024-01-15 22:09:25 +0200

    build(flake): refactor to use flake-parts

--- flake.nix
+++ flake.nix
@@ -7,76 +7,129 @@
       url = "github:nialov/nix-extra";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
-    pre-commit-hooks = { url = "github:cachix/pre-commit-hooks.nix"; };
+    flake-parts.follows = "nix-extra/flake-parts";
+    flake-utils.follows = "nix-extra/flake-utils";
+    pre-commit-hooks.follows = "nix-extra/pre-commit-hooks";
   };
   nixConfig.extra-substituters = [ "https://fractopo.cachix.org" ];
   nixConfig.extra-trusted-public-keys =
     [ "fractopo.cachix.org-1:Eo5bn5VTQSp4J3+XQnGYlq4dH/2ibKjxrs5n9qKl9Ms=" ];
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    let inherit (nixpkgs) lib;
-    in lib.recursiveUpdate
-    (flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (system:
-      let
-        # Initialize nixpkgs for system
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      vanillaPerSystem = { };
+      vanillaFlake = {
 
-        devShellPackages = with pkgs; [
-          pre-commit
-          pandoc
-          poetry-with-c-tooling
-          # Supported python versions
-          python38
-          python39
-          python310
-          python311
-        ];
-
-      in {
-        checks = lib.recursiveUpdate {
-          preCommitCheck = inputs.pre-commit-hooks.lib.${system}.run
-            (import ././pre-commit.nix { inherit pkgs; });
-
-        } self.packages."${system}";
-        packages = {
-          inherit (pkgs)
-            sync-git-tag-with-poetry resolve-version update-changelog
-            pre-release fractopo;
-          poetry-run = pkgs.poetry-run-fractopo;
-          # django not supported for python <3.10
-          # fractopo38 = pkgs.python38Packages.fractopo;
-          # fractopo39 = pkgs.python39Packages.fractopo;
-          fractopo310 = pkgs.python310Packages.fractopo;
-          fractopo311 = pkgs.python311Packages.fractopo;
-          fractopo-documentation = pkgs.fractopo.passthru.documentation;
-          default = self.packages."${system}".fractopo;
-        };
-        devShells = {
-          default = pkgs.mkShell {
-            packages = devShellPackages;
-            inherit (self.checks.${system}.preCommitCheck) shellHook;
-          };
-        };
-      })) {
-
-        overlays.default = inputs.nixpkgs.lib.composeManyExtensions [
-          inputs.nix-extra.overlays.utils
-          (final: prev: {
-            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-              (python-final: _: {
-                "fractopo" = python-final.callPackage ./default.nix { };
+      };
+      flakePart = inputs.flake-parts.lib.mkFlake { inherit inputs; }
+        ({ inputs, ... }: {
+          systems = [ "x86_64-linux" ];
+          imports = [
+            inputs.nix-extra.flakeModules.custom-pre-commit-hooks
+            inputs.nix-extra.flakeModules.poetryDevshell
+          ];
+          flake = {
+            overlays.default = inputs.nixpkgs.lib.composeManyExtensions [
+              inputs.nix-extra.overlays.utils
+              (final: prev: {
+                pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+                  (python-final: _: {
+                    "fractopo" = python-final.callPackage ./default.nix { };
+                  })
+                ];
+                inherit (final.python3Packages) fractopo;
+                poetry-run-fractopo = final.poetry-run.override {
+                  pythons = with prev; [
+                    python38
+                    python39
+                    python310
+                    python311
+                  ];
+                };
               })
             ];
-            inherit (final.python3Packages) fractopo;
-            poetry-run-fractopo = final.poetry-run.override {
-              pythons = with prev; [ python38 python39 python310 python311 ];
+          };
+          perSystem = { config, system, pkgs, lib, self', ... }:
+            let
+              mkNixpkgs = nixpkgs:
+                import nixpkgs {
+                  inherit system;
+                  overlays = [ self.overlays.default ];
+                  config = { allowUnfree = true; };
+                };
+
+            in {
+              _module.args.pkgs = mkNixpkgs inputs.nixpkgs;
+              checks = self'.packages;
+              packages = {
+                inherit (pkgs) fractopo;
+                poetry-run = pkgs.poetry-run-fractopo;
+                # django not supported for python <3.10
+                # fractopo38 = pkgs.python38Packages.fractopo;
+                # fractopo39 = pkgs.python39Packages.fractopo;
+                fractopo310 = pkgs.python310Packages.fractopo;
+                fractopo311 = pkgs.python311Packages.fractopo;
+                fractopo-documentation = pkgs.fractopo.passthru.documentation;
+                default = self'.packages.fractopo;
+              };
+              devShells.default = self'.devShells.poetry-devshell.overrideAttrs
+                (prevAttrs: {
+                  buildInputs = prevAttrs.buildInputs ++ [
+                    pkgs.poetry-run-fractopo
+
+                  ];
+                });
+              pre-commit = {
+                check.enable = true;
+                settings.hooks = {
+
+                  nixfmt.enable = true;
+                  black.enable = true;
+                  flake8.enable = true;
+                  isort = { enable = true; };
+                  statix = { enable = true; };
+                  deadnix.enable = true;
+                  editorconfig-checker.enable = true;
+                  commitizen.enable = true;
+                  sync-git-tag-with-poetry = {
+                    enable = true;
+                    name = "sync-git-tag-with-poetry";
+                    description = "sync-git-tag-with-poetry";
+                    entry = ''
+                      ${pkgs.sync-git-tag-with-poetry}/bin/sync-git-tag-with-poetry
+                    '';
+                    # stages = [ "push" "manual" ];
+                    pass_filenames = false;
+                  };
+                  trim-trailing-whitespace = {
+                    enable = true;
+
+                  };
+                  check-added-large-files = { enable = true; };
+                  rstcheck = {
+                    enable = true;
+                    raw = { args = [ "-r" "docs_src" ]; };
+                  };
+                  cogapp = {
+                    enable = true;
+                    files = "(README.rst|docs_src/index.rst)";
+                    pass_filenames = false;
+                    raw = {
+                      args = lib.mkAfter [ "docs_src/index.rst" ];
+                      always_run = true;
+                    };
+                  };
+                  nbstripout = { enable = true; };
+                };
+              };
             };
-          })
-        ];
-      };
+
+        });
+    in lib.foldl' lib.recursiveUpdate { } [
+      vanillaPerSystem
+      vanillaFlake
+      flakePart
+    ];
 
 }
