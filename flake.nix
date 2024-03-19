bd88576fa7c389f9b3d75ffa8010f1bbf457eb75
nialov <nikolasovaskainen@gmail.com>
2024-03-19 16:31:05 +0200

    build: use flake-parts and nix-extra
    Reduces nix code repetition between fractopo and nix-extra.

--- flake.nix
+++ flake.nix
@@ -1,82 +1,24 @@
 {
-  description = "fractopo: A Python package for fracture network analysis";
+  description = "Description for the project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-extra = {
-      url = "github:nialov/nix-extra";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-utils.url = "github:numtide/flake-utils";
-    pre-commit-hooks = { url = "github:cachix/pre-commit-hooks.nix"; };
+    nix-extra = { url = "github:nialov/nix-extra"; };
+    nixpkgs.follows = "nix-extra/nixpkgs";
+    flake-parts.follows = "nix-extra/flake-parts";
   };
-  nixConfig.extra-substituters = [ "https://fractopo.cachix.org" ];
-  nixConfig.extra-trusted-public-keys =
-    [ "fractopo.cachix.org-1:Eo5bn5VTQSp4J3+XQnGYlq4dH/2ibKjxrs5n9qKl9Ms=" ];
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    let inherit (nixpkgs) lib;
-    in lib.recursiveUpdate
-    (flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (system:
-      let
-        # Initialize nixpkgs for system
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
+  outputs = inputs:
+    let
+      flakePart = inputs.flake-parts.lib.mkFlake { inherit inputs; }
+        ({ self, inputs, ... }: {
+          systems = [ "x86_64-linux" ];
+          imports = [
+            inputs.nix-extra.flakeModules.custom-pre-commit-hooks
+            ./per-system.nix
+          ];
+          flake = { inherit self; };
+        });
 
-        devShellPackages = with pkgs; [
-          pre-commit
-          pandoc
-          poetry-with-c-tooling
-          # Supported python versions
-          python38
-          python39
-          python310
-          python311
-        ];
-
-      in {
-        checks = lib.recursiveUpdate {
-          preCommitCheck = inputs.pre-commit-hooks.lib.${system}.run
-            (import ././pre-commit.nix { inherit pkgs; });
-
-        } self.packages."${system}";
-        packages = {
-          inherit (pkgs)
-            sync-git-tag-with-poetry resolve-version update-changelog
-            pre-release fractopo;
-          poetry-run = pkgs.poetry-run-fractopo;
-          # django not supported for python <3.10
-          # fractopo38 = pkgs.python38Packages.fractopo;
-          # fractopo39 = pkgs.python39Packages.fractopo;
-          fractopo310 = pkgs.python310Packages.fractopo;
-          fractopo311 = pkgs.python311Packages.fractopo;
-          fractopo-documentation = pkgs.fractopo.passthru.documentation;
-          default = self.packages."${system}".fractopo;
-        };
-        devShells = {
-          default = pkgs.mkShell {
-            packages = devShellPackages;
-            inherit (self.checks.${system}.preCommitCheck) shellHook;
-          };
-        };
-      })) {
-
-        overlays.default = inputs.nixpkgs.lib.composeManyExtensions [
-          inputs.nix-extra.overlays.utils
-          (final: prev: {
-            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-              (python-final: _: {
-                "fractopo" = python-final.callPackage ./default.nix { };
-              })
-            ];
-            inherit (final.python3Packages) fractopo;
-            poetry-run-fractopo = final.poetry-run.override {
-              pythons = with prev; [ python38 python39 python310 python311 ];
-            };
-          })
-        ];
-      };
+    in flakePart;
 
 }
---
+++ per-system.nix
@@ -0,0 +1,94 @@
+({ self, inputs, ... }:
+
+  {
+    perSystem = { self', config, system, pkgs, lib, ... }:
+      let
+        mkNixpkgs = nixpkgs:
+          import nixpkgs {
+            inherit system;
+            overlays = [
+
+              inputs.nix-extra.overlays.default
+
+              (final: prev: {
+                pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+                  (_: pythonPrev: {
+                    "fractopo" = pythonPrev.fractopo.overridePythonAttrs
+                      # Test with local source
+                      (_: { src = self.outPath; });
+                  })
+                ];
+                inherit (final.python3Packages) fractopo;
+              })
+
+            ];
+            config = { allowUnfree = true; };
+          };
+
+      in {
+        _module.args.pkgs = mkNixpkgs inputs.nixpkgs;
+        devShells = let
+          devShellPackages = with pkgs; [
+            pre-commit
+            pandoc
+            poetry-with-c-tooling
+            # Supported python versions
+            python38
+            python39
+            python310
+            python311
+          ];
+
+        in {
+          default = pkgs.mkShell {
+            packages = devShellPackages;
+            shellHook = config.pre-commit.installationScript + ''
+              export PROJECT_DIR="$PWD"
+            '';
+          };
+
+        };
+
+        pre-commit = {
+          check.enable = true;
+          settings.hooks = {
+            nixfmt.enable = true;
+            black.enable = true;
+            black-nb.enable = true;
+            nbstripout.enable = true;
+            isort = { enable = true; };
+            shellcheck.enable = true;
+            statix.enable = true;
+            deadnix.enable = true;
+            rstcheck.enable = true;
+            trim-trailing-whitespace.enable = true;
+            check-added-large-files.enable = true;
+            sync-git-tag-with-poetry.enable = true;
+            cogapp = {
+              enable = true;
+              raw = { args = [ "docs_src/index.rst" ]; };
+
+            };
+            yamllint = {
+              enable = true;
+              raw = { args = lib.mkBefore [ "-d" "relaxed" ]; };
+            };
+            commitizen.enable = true;
+            ruff = { enable = true; };
+          };
+
+        };
+        packages = {
+
+          inherit (pkgs) fractopo poetry-run;
+          fractopo310 = pkgs.python310Packages.fractopo;
+          fractopo311 = pkgs.python311Packages.fractopo;
+          fractopo-documentation =
+            self'.packages.fractopo.passthru.documentation.doc;
+          default = self'.packages.fractopo;
+
+        };
+        checks = self'.packages;
+      };
+
+  })
