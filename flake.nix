63034134c2a4e92713d00b49b3674f43c61edf2b
nialov <nikolasovaskainen@gmail.com>
2024-11-01 11:11:34 +0200

    build(flake): use unstable nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nix-extra = { url = "github:nialov/nix-extra"; };
-    nixpkgs.follows = "nix-extra/nixpkgs-fractopo";
+    nixpkgs.follows = "nix-extra/nixpkgs";
     flake-parts.follows = "nix-extra/flake-parts";
   };
 
