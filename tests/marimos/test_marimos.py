92d1dbecb0e547e8d9aa4631cda7251ac7aec53a
nialov <nikolasovaskainen@gmail.com>
2025-02-05 16:44:52 +0200

    test(marimos): rename and add network tests

--- tests/marimos/test_marimos.py
+++ tests/marimos/test_marimos.py
@@ -12,6 +12,7 @@ SAMPLE_DATA_DIR = Path(__file__).parent.parent.joinpath("sample_data/")
 VALIDATION_NOTEBOOK = Path(__file__).parent.parent.parent.joinpath(
     "marimos/validation.py"
 )
+NETWORK_NOTEBOOK = Path(__file__).parent.parent.parent.joinpath("marimos/network.py")
 PYTHON_INTERPRETER = sys.executable
 
 check_python_call = partial(
@@ -28,12 +29,14 @@ check_python_call = partial(
     },
 )
 
-
-@pytest.mark.xfail(
+pytest_mark_xfail_windows_flaky = pytest.mark.xfail(
     sys.platform == "win32",
     reason="Subprocess call is flaky in Windows",
     raises=subprocess.CalledProcessError,
 )
+
+
+@pytest_mark_xfail_windows_flaky
 @pytest.mark.parametrize(
     "traces_path,area_path,name",
     [
@@ -64,11 +67,7 @@ def test_validation_cli(traces_path: str, area_path: str, name: str):
     )
 
 
-@pytest.mark.xfail(
-    sys.platform == "win32",
-    reason="Subprocess call is flaky in Windows",
-    run=False,
-)
+@pytest_mark_xfail_windows_flaky
 @pytest.mark.parametrize(
     "args,raises",
     [
@@ -78,3 +77,34 @@ def test_validation_cli(traces_path: str, area_path: str, name: str):
 def test_validation_cli_args(args, raises):
     with raises:
         check_python_call([PYTHON_INTERPRETER, VALIDATION_NOTEBOOK.as_posix(), *args])
+
+
+@pytest_mark_xfail_windows_flaky
+@pytest.mark.parametrize(
+    "traces_path,area_path,name",
+    [
+        # (
+        #     SAMPLE_DATA_DIR.joinpath("hastholmen_traces.geojson").as_posix(),
+        #     SAMPLE_DATA_DIR.joinpath("hastholmen_area.geojson").as_posix(),
+        #     "hastholmen",
+        # ),
+        (
+            tests.kb7_trace_100_path.as_posix(),
+            tests.kb7_area_path.as_posix(),
+            "kb7",
+        ),
+    ],
+)
+def test_network_cli(traces_path: str, area_path: str, name: str):
+    args = [
+        "--traces-path",
+        traces_path,
+        "--area-path",
+        area_path,
+        "--name",
+        name,
+    ]
+
+    check_python_call(
+        [PYTHON_INTERPRETER, NETWORK_NOTEBOOK.as_posix(), *args],
+    )
