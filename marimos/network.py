32990c555d05667224a457a2e978311a3a025fe1
nialov <nikolasovaskainen@gmail.com>
2025-03-11 11:06:39 +0200

    refactor(marimos): move code to utils.py

--- marimos/network.py
+++ marimos/network.py
@@ -1,3 +1,8 @@
+# /// script
+# [tool.marimo.runtime]
+# on_cell_change = "autorun"
+# ///
+
 import marimo
 
 __generated_with = "0.9.1"
@@ -9,12 +14,14 @@ def _():
     import logging
     import tempfile
     import zipfile
+    from functools import partial
     from io import BytesIO
     from pathlib import Path
 
     import geopandas as gpd
     import marimo as mo
     import pyogrio
+    import utils
 
     import fractopo.general
     import fractopo.tval.trace_validation
@@ -26,8 +33,10 @@ def _():
         gpd,
         logging,
         mo,
+        partial,
         pyogrio,
         tempfile,
+        utils,
         zipfile,
     )
 
@@ -43,12 +52,14 @@ def _(mo):
     input_circular_target_area = mo.ui.switch(False)
     input_determine_branches_nodes = mo.ui.switch(True)
     input_truncate_traces = mo.ui.switch(True)
+    input_debug = mo.ui.switch(False)
     input_button = mo.ui.run_button()
     return (
         input_area_file,
         input_area_layer_name,
         input_button,
         input_circular_target_area,
+        input_debug,
         input_determine_branches_nodes,
         input_name,
         input_snap_threshold,
@@ -64,6 +75,7 @@ def __(
     input_area_layer_name,
     input_button,
     input_circular_target_area,
+    input_debug,
     input_determine_branches_nodes,
     input_name,
     input_snap_threshold,
@@ -88,6 +100,7 @@ def __(
         mo.md(f"Is the target area a circle? {input_circular_target_area}"),
         mo.md(f"Determine branches and nodes? {input_determine_branches_nodes}"),
         mo.md(f"Truncate traces to target area? {input_truncate_traces}"),
+        mo.md(f"Enable verbose debug output? {input_debug}"),
         mo.md(f"Press to (re)start analysis: {input_button}"),
     ]
 
@@ -97,9 +110,7 @@ def __(
 
 @app.cell
 def _(
-    Path,
     fractopo,
-    gpd,
     input_area_file,
     input_area_layer_name,
     input_button,
@@ -110,79 +121,23 @@ def _(
     input_traces_file,
     input_truncate_traces,
     mo,
-    pyogrio,
+    utils,
 ):
     def execute():
         cli_args = mo.cli_args()
         if len(cli_args) != 0:
-            cli_traces_path = Path(cli_args.get("traces-path"))
-            cli_area_path = Path(cli_args.get("area-path"))
-
-            name = cli_args.get("name") or cli_traces_path.stem
-
-            driver = pyogrio.detect_write_driver(cli_traces_path.name)
-
-            traces_gdf = gpd.read_file(cli_traces_path, driver=driver)
-            area_gdf = gpd.read_file(cli_area_path, driver=driver)
-            snap_threshold_str = cli_args.get("snap-threshold")
-            if snap_threshold_str is None:
-                snap_threshold = (
-                    fractopo.tval.trace_validation.Validation.SNAP_THRESHOLD
-                )
-            else:
-                snap_threshold = float(snap_threshold_str)
+            name, driver, traces_gdf, area_gdf, snap_threshold = (
+                utils.parse_network_cli_args(cli_args)
+            )
         else:
             mo.stop(not input_button.value)
-
-            trace_layer_name = (
-                input_trace_layer_name.value
-                if input_trace_layer_name.value != ""
-                else None
-            )
-            area_layer_name = (
-                input_area_layer_name.value
-                if input_area_layer_name.value != ""
-                else None
-            )
-
-            driver = pyogrio.detect_write_driver(input_traces_file.name())
-            print(f"Detected driver: {driver}")
-
-            print(
-                f"Trace layer name: {trace_layer_name}"
-                if trace_layer_name is not None
-                else "No layer specified"
-            )
-            traces_gdf = gpd.read_file(
-                input_traces_file.contents(),
-                layer=trace_layer_name,
-                # , driver=driver
-            )
-            print(
-                f"Area layer name: {area_layer_name}"
-                if area_layer_name is not None
-                else "No layer specified"
-            )
-            area_gdf = gpd.read_file(
-                input_area_file.contents(),
-                layer=area_layer_name,
-                # , driver=driver
-            )
-
-            snap_threshold = float(input_snap_threshold.value)
-            name = (
-                Path(input_traces_file.name()).stem
-                if trace_layer_name is None
-                else trace_layer_name
-            )
-
-            print(
-                str.join(
-                    "\n",
-                    [
-                        f"Snap threshold: {snap_threshold}",
-                        f"Name: {name}",
-                    ],
+            name, driver, traces_gdf, area_gdf, snap_threshold = (
+                utils.parse_network_app_args(
+                    input_trace_layer_name,
+                    input_area_layer_name,
+                    input_traces_file,
+                    input_area_file,
+                    input_snap_threshold,
                 )
             )
 
@@ -202,18 +157,29 @@ def _(
 
 
 @app.cell
-def _(execute, logging, mo):
-    with mo.redirect_stderr():
-        with mo.redirect_stdout():
-            try:
-                network, name = execute()
-                execute_exception = None
-            except Exception as exc:
-                logging.error("Failed to analyze trace data.", exc_info=True)
-                network = None
-                name = None
-                execute_exception = exc
-    return execute_exception, name, network
+def _(execute, input_debug, mo, partial, utils):
+    execute_results, execute_exception, execute_stderr_and_stdout = (
+        utils.capture_function_outputs(execute)
+    )
+    report_output = partial(
+        mo.output.replace,
+        "\n".join([execute_stderr_and_stdout, str(execute_exception)]),
+    )
+    if input_debug.value or execute_exception is not None:
+        report_output()
+    if execute_exception is not None:
+        network = None
+        name = None
+    else:
+        network, name = execute_results
+    return (
+        execute_exception,
+        execute_results,
+        execute_stderr_and_stdout,
+        name,
+        network,
+        report_output,
+    )
 
 
 @app.cell
@@ -232,70 +198,22 @@ def _(mo, network):
 
 
 @app.cell
-def __(
-    BytesIO,
-    Path,
-    input_determine_branches_nodes,
-    mo,
-    name,
-    network,
-    tempfile,
-    zipfile,
-):
-    def to_file():
-        if network is not None:
-            with tempfile.TemporaryDirectory() as tmp_dir:
-                tmp_dir_path = Path(tmp_dir)
-
-                # Create and write plots to tmp_dir
-                if input_determine_branches_nodes.value:
-                    network.export_network_analysis(output_path=tmp_dir_path)
-                else:
-                    _, fig, _ = network.plot_trace_azimuth()
-                    fig.savefig(tmp_dir_path / "trace_azimuth.png", bbox_inches="tight")
-                    _, fig, _ = network.plot_trace_lengths()
-                    fig.savefig(tmp_dir_path / "trace_lengths.png", bbox_inches="tight")
-
-                zip_io = BytesIO()
-
-                # Open an in-memory zip file
-                with zipfile.ZipFile(zip_io, mode="a") as zip_file:
-                    for path in tmp_dir_path.rglob("*"):
-                        # Do not add directories, only files
-                        if path.is_dir():
-                            continue
-                        path_rel = path.relative_to(tmp_dir_path)
-
-                        # Write file in-memory to zip file
-                        zip_file.write(path, arcname=path_rel)
-
-            # Move to start of file
-            zip_io.seek(0)
-
-            download_element = mo.download(
-                data=zip_io,
-                filename=f"{name}.zip",
-                mimetype="application/octet-stream",
+def __(input_determine_branches_nodes, mo, name, network, partial, utils):
+    download_element, to_file_exception, to_file_stderr_and_stdout = (
+        utils.capture_function_outputs(
+            partial(
+                utils.network_results_to_download_element,
+                network=network,
+                determine_branches_nodes=input_determine_branches_nodes.value,
+                name=name,
             )
-        else:
-            download_element = None
-        return download_element
-
-    return (to_file,)
-
-
-@app.cell
-def __(logging, mo, to_file):
-    with mo.redirect_stderr():
-        with mo.redirect_stdout():
-            try:
-                download_element = to_file()
-                to_file_exception = None
-            except Exception as exc:
-                logging.error("Failed to write results.", exc_info=True)
-                to_file_exception = exc
-                download_element = None
-    return download_element, to_file_exception
+        )
+    )
+    if to_file_exception is not None:
+        mo.output.replace(
+            "\n".join([to_file_stderr_and_stdout, str(to_file_exception)])
+        )
+    return download_element, to_file_exception, to_file_stderr_and_stdout
 
 
 @app.cell
