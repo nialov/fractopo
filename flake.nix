44087325dc0eb874032eebb300660f6e60606382
nialov <nikolasovaskainen@gmail.com>
2025-08-01 10:34:20 +0300

    ci: update actions-nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,10 @@
     nix-extra = { url = "github:nialov/nix-extra"; };
     nixpkgs.follows = "nix-extra/nixpkgs";
     flake-parts.follows = "nix-extra/flake-parts";
-    actions-nix = { url = "github:nialov/actions.nix"; };
+    actions-nix = {
+      url = "github:nialov/actions.nix";
+      inputs.git-hooks.follows = "nix-extra/pre-commit-hooks";
+    };
   };
 
   outputs = inputs: (import ./nix) inputs;
