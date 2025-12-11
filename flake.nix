a22a2b08316ca6ed902d5ababad724341a434787
nialov <nikolasovaskainen@gmail.com>
2025-12-11 10:15:41 +0200

    build(flake): update inputs

--- flake.nix
+++ flake.nix
@@ -2,12 +2,15 @@
   description = "Description for the project";
 
   inputs = {
-    nix-extra = { url = "github:nialov/nix-extra"; };
+    nix-extra = {
+      url = "github:nialov/nix-extra";
+    };
     nixpkgs.follows = "nix-extra/nixpkgs";
     flake-parts.follows = "nix-extra/flake-parts";
     actions-nix = {
       url = "github:nialov/actions.nix";
-      inputs.git-hooks.follows = "nix-extra/pre-commit-hooks";
+      inputs.git-hooks.follows = "nix-extra/git-hooks";
+      inputs.flake-parts.follows = "nix-extra/flake-parts";
     };
   };
 
