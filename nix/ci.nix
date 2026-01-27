032074a22335b9ed615f98303c02491f136cd3cc
nialov <nikolasovaskainen@gmail.com>
2026-01-27 13:38:30 +0200

    build: add back python 3.10 support

--- nix/ci.nix
+++ nix/ci.nix
@@ -60,6 +60,7 @@ in
             };
             poetry = {
               strategy.matrix.python-version = [
+                "3.10"
                 "3.11"
                 "3.12"
                 "3.13"
