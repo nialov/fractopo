12ed9596de79eb3632dc628b4e0034922a608539
nialov <nikolasovaskainen@gmail.com>
2026-08-21 09:30:28 +0300

    fix(typing): use untyped array

--- fractopo/typing.py
+++ fractopo/typing.py
@@ -6,7 +6,6 @@ import geopandas as gpd
 import numpy as np
 from beartype.typing import Annotated
 from beartype.vale import Is
-from numpy.typing import NDArray
 from shapely.geometry import LineString, Point
 
 GeoDataFrameWithLineStrings = Annotated[
@@ -21,26 +20,26 @@ GeoDataFrameWithPoints = Annotated[
 
 
 NDArrayWithDips = Annotated[
-    NDArray,
+    np.ndarray,
     Is[lambda arr: all((arr >= 0.0) & (arr <= 90.0))],
 ]
 
 NDArrayWithDipDirections = Annotated[
-    NDArray,
+    np.ndarray,
     Is[lambda arr: all((arr >= 0.0) & (arr <= 360.0)) and arr.ndim == 1],
 ]
 
 NDArrayWithAxialAzimuths = Annotated[
-    NDArray,
+    np.ndarray,
     Is[lambda arr: arr.ndim == 1 and np.all((arr >= 0.0) & (arr <= 180.0))],
 ]
 
 NDArrayWithPositives = Annotated[
-    NDArray,
+    np.ndarray,
     Is[lambda arr: arr.ndim == 1 and np.all(arr > 0.0)],
 ]
 
-NDArray1D = Annotated[NDArray, Is[lambda arr: arr.ndim == 1]]
+NDArray1D = Annotated[np.ndarray, Is[lambda arr: arr.ndim == 1]]
 
 NDArray1DNotEmpty = Annotated[NDArray1D, Is[lambda arr: arr.size > 0]]
 
